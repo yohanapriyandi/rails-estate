02b02d4e77635c6ad5552974d0cfadaf2987a957
Yohan Apriyandi <yohanapriyandi89@gmail.com>
2021-08-10 13:07:38 +0700

    update schema.rb after create migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_07_052314) do
+ActiveRecord::Schema.define(version: 2021_08_10_042337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2021_08_07_052314) do
     t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "parking_spaces", default: 0
+    t.text "details"
     t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
