db78aad6ace5b2e8d902cfbf334a1122269ab97c
Yohan Apriyandi <cepoangkuningan@gmail.com>
2021-08-13 07:22:30 +0700

    redirect to dashboard when agent signin

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,8 @@
 class PublicController < ApplicationController
   def main
+    if account_signed_in?
+      redirect_to dashboard_path and return
+    end
     @properties = Property.latest
   end
 end
