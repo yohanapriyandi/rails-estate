4aa68f8d62d43794f238ad3a0591cc9e0d323140
Yohan Apriyandi <cepoangkuningan@gmail.com>
2021-08-13 07:16:48 +0700

    add action to show profile picture

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    def profile_picture account, width = 100
+        thumb = account.image.present? ? account.image.thumb.url : "placeholdler.png"
+        image_tag thumb, width: width, class: "profile-pic rounded-circle"
+    end
 end
