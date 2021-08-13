9505d008aafabd5a8b108fb3d45f629819519cc9
Yohan Apriyandi <cepoangkuningan@gmail.com>
2021-08-13 19:49:06 +0700

    add for_sale & available_date on property_params

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -73,6 +73,6 @@ class PropertiesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def property_params
-      params.require(:property).permit(:name, :address, :price, :rooms, :bathrooms, :parking_spaces, :details, :photo, :photo_cache)
+      params.require(:property).permit(:name, :address, :price, :rooms, :bathrooms, :parking_spaces, :for_sale, :available_date, :details, :photo, :photo_cache)
     end
 end
