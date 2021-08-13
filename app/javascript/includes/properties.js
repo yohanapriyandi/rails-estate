310a4d62aecfab263433e2ce925de1f2bcad1d4d
Yohan Apriyandi <cepoangkuningan@gmail.com>
2021-08-13 13:51:31 +0700

    fixing midified of file

--- app/javascript/includes/properties.js
+++ app/javascript/includes/properties.js
@@ -3,5 +3,10 @@ $(function(){
         var details = $("#prop-details");
         // console.log('test');
         details.toggleClass("open");
+        if (details.hasClass("open")) {
+            $(this).text("Show less");
+        } else {
+            $(this).text("Show more");
+        }
     })        
 });
