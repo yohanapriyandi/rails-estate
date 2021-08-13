7a9181290b476a60ce6305c55df3958961992b7b
Yohan Apriyandi <cepoangkuningan@gmail.com>
2021-08-13 09:56:45 +0700

    implement flash_messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,20 @@ module ApplicationHelper
         thumb = account.image.present? ? account.image.thumb.url : "placeholdler.png"
         image_tag thumb, width: width, class: "profile-pic rounded-circle"
     end
+
+    def flash_notifications
+        flash_messages = []
+
+        flash.each do |type, message|
+            type = 'success' if type == 'notice'
+            type = 'error' if type == 'alert' || type == 'danger'
+            text = "toastr['#{type}']('#{message}');"
+            flash_messages << text.html_safe unless message.blank?
+        end
+
+        "<script>
+            $(function(){ #{ flash_messages.join("\n") } } );
+        </script>".html_safe if flash_messages.any?
+
+    end
 end
