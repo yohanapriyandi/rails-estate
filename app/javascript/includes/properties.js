2510ead12462a23cd5389a5cc4f4c438669a9cbb
Yohan Apriyandi <cepoangkuningan@gmail.com>
2021-08-19 14:33:03 +0700

    add ajax function for send message to agent

--- app/javascript/includes/properties.js
+++ app/javascript/includes/properties.js
@@ -8,5 +8,38 @@ $(function(){
         } else {
             $(this).text("Show more");
         }
-    });    
+    });
+
+    $("#send-email").on("click", function(){
+        var agent_id = $("#agent_id").val(),
+            first_name = $("#message-first-name").val(),
+            last_name = $("#message-last-name").val(),
+            email = $("#message-email").val(),
+            message = $("#message-text").val();
+
+        $.ajax({
+            url: "/agent/message",
+            method: "POST",
+            dataType: "json",
+            data: {
+                agent_id: agent_id,
+                first_name: first_name,
+                last_name: last_name,
+                email: email,
+                message: message,
+            },
+            success: function(data){
+                $('#contact-modal form').remove();
+                $('#send-email').remove();
+                $('#contact-modal .modal-body').text("Your message has been sent succesfully!");                
+            }
+        });
+    });
+
+    // modal
+        
+      $("#contact_modal").on("click", function(event){
+        var button = $(event.relatedTarget);
+        var modal = $(this);
+      });        
 });
