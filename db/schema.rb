348baa27e54670a12b06ba8b9d353ec9987a7d7e
Yohan Apriyandi <cepoangkuningan@gmail.com>
2021-08-19 14:42:23 +0700

    add admin field to accounts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_131738) do
+ActiveRecord::Schema.define(version: 2021_08_19_074017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2021_08_13_131738) do
     t.string "image"
     t.string "company"
     t.string "phone"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
