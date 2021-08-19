bea4a8465478b521f0975a1156ca7f08fe20f082
Yohan Apriyandi <cepoangkuningan@gmail.com>
2021-08-19 14:34:42 +0700

    add routes for send message to agent

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :accounts
   get '/dashboard' => 'dashboard#index', as: :dashboard
   get '/profile/:id' => 'dashboard#profile', as: :profile
+  post '/agent/message' => 'properties#email_agent', as: :email_agent
   # get 'dashboard/properties'
   # get 'dashboard/reports'  
   root to: 'public#main'
